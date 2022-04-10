c77937cdcdfe6edd1f989174bd516c48195a6ac1
oohyun15 <sakiss4774@gmail.com>
2022-04-10 18:53:28 +0900

    :wrench: convention

--- Formular/ballantine.rb
+++ Formular/ballantine.rb
@@ -1,9 +1,9 @@
 class Ballantine < Formula
   desc "describe your commits"
+  homepage ""
   url "https://github.com/oohyun15/ballantine/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "441bbbb719ef1e6eb078cdb7d4c89413b3a0ac0e2625e551f81c02ea15aec113"
   license "MIT"
-  homepage ""
   version "0.0.1"
 
   def install

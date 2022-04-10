fde7820e418ece68023e89c94588aaca7b6267c0
oohyun15 <sakiss4774@gmail.com>
2022-04-10 18:33:49 +0900

    :fire: Remove comment

--- Formular/ballantine.rb
+++ Formular/ballantine.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ballantine < Formula
   desc "describe your commits"
   url "https://github.com/oohyun15/ballantine/archive/refs/tags/v0.0.1.tar.gz"

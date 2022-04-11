34d9ddfa5b154af6ae488684080c6c1b32267267
oohyun15 <sakiss4774@gmail.com>
2022-04-12 03:10:27 +0900

    goes to v0.0.5

--- Formula/ballantine.rb
+++ Formula/ballantine.rb
@@ -1,8 +1,8 @@
 class Ballantine < Formula
   desc "Describe your commits"
   homepage "https://github.com/oohyun15/ballantine"
-  url "https://github.com/oohyun15/ballantine/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "9ecc249ab6ae5aefde291b4424cfdca4c090da512b2c27518c9ab523db69c0d6"
+  url "https://github.com/oohyun15/ballantine/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "8a433e6459fba394c13fd7ce5cbc3602171179b747eb75c51183b7a0555a9784"
   license "MIT"
 
   def install

23791b3adc6f53fd79f00a66a6615e3f93994dc1
oohyun15 <sakiss4774@gmail.com>
2023-01-31 17:37:41 +0000

    :arrow_up: Upgrade to v0.1.4

--- Formula/ballantine.rb
+++ Formula/ballantine.rb
@@ -1,8 +1,8 @@
 class Ballantine < Formula
   desc "Describe your commits"
   homepage "https://github.com/oohyun15/ballantine"
-  url "https://github.com/oohyun15/ballantine/archive/refs/tags/v0.1.4-beta4.tar.gz"
-  sha256 "c59508a1db24126b1bdb9a6a4097bbae9419969e350c7a54a07540c39b02af03"
+  url "https://github.com/oohyun15/ballantine/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "30077bd05c334f4ffbe4341d393d2316fb64f2d4c96f1f0971149c7f9108b0e6"
   license "MIT"
 
   depends_on "git"
